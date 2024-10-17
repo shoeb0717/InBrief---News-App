3cea1694a2685464639f6ad740398ad3da6e78ec
shoeb0717 <shoeb0717@gmail.com>
2024-10-17 09:31:45 +0530

    second commit

--- app/src/main/java/com/learngram/mvvmnewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/learngram/mvvmnewsapp/api/NewsAPI.kt
@@ -1,7 +1,7 @@
 package com.learngram.mvvmnewsapp.api
 
 import com.learngram.mvvmnewsapp.Models.NewsResponse
-import com.learngram.mvvmnewsapp.utils.Constants.Companion.API_KEY
+import com.learngram.mvvmnewsapp.utils.ApiKey.Companion.API_KEY
 import retrofit2.Response
 import retrofit2.http.Query
 import retrofit2.http.GET
--- app/src/main/java/com/learngram/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/learngram/mvvmnewsapp/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.learngram.mvvmnewsapp.utils
 class Constants {
 
     companion object{
-        const val API_KEY="702b44682333426a8b2259cdc62f134a"
         const val BASE_URL="https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY=500L
         const val QUERY_PAGE_SIZE=20
